07eeeb271a185098430388b3d7083eceecf4041d
yorkaespinosa <yorka.espinosa@gmail.com>
2022-04-23 20:32:08 -0400

    mdif
    prueba

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//scrolltop para menu 
+$('html, body').animate({
+    scrollTop: $("#seccion-01").offset().top
+  }, 550);
+  $('html, body').animate({
+    scrollTop: $("#seccion-02").offset().top
+  }, 550);
+  $('html, body').animate({
+    scrollTop: $("#seccion-03").offset().top
+  }, 550);
+  $('html, body').animate({
+    scrollTop: $("#seccion-00").offset().top
+  }, 550);
